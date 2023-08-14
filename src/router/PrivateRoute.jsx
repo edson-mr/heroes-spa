0c81184a7d562d3c76b8116bd969fe7e3ec4740a
edson <eamramirez95@gmail.com>
2023-08-13 23:01:33 -0500

    modified

--- src/router/PrivateRoute.jsx
+++ src/router/PrivateRoute.jsx
@@ -5,8 +5,10 @@ import { Navigate, useLocation } from "react-router-dom";
 
 export const PrivateRoute = ({ children }) => {
   const { logged } = useContext(AuthContext);
-  const {pathname, search} = useLocation()
-  console.log(pathname, search)
+  const location = useLocation()
+  // console.log(pathname, search)
+  console.log(location)
+  const {pathname, search} = location
   const lastPath = pathname+ search
   localStorage.setItem('lastPath', lastPath)
 
